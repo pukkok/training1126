67736476f4f0752433ab06acef3f7073a58b0305
pukkok <krystalgggg@gmail.com>
2024-11-26 11:27:51 +0900

    setTimeout 기본동작 확인

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log('hello world!')
+// const first = () => setTimeout(() => {
+//     console.log('3초')
+//     second()
+// }, 3000)
+
+// const second = () => setTimeout(() => console.log('1초 뒤'), 1000)
+
+// first()
+
+let counter = 0
+
+const timer = () => setTimeout(() => {
+    if(counter < 5) {
+        // console.log('동작 테스트')
+        timer()
+        counter++
+        console.log(counter)
+    } else {
+        // clearTimeout(timer)
+    }
+
+}, 1000)
+
+timer()
