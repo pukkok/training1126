4f0273828d220a298965734c8fbbc0cc211a7b68
pukkok <krystalgggg@gmail.com>
2024-11-26 15:01:18 +0900

    urlChecker 작동 확인 완료

--- src/server.js
+++ src/server.js
@@ -1,8 +1,16 @@
 import http from 'http'
+import UrlChecker from './fileReader/UrlChecker.js'
 
 const server = http.createServer((req, res) => {
+
+
     if(req.method === 'GET') {
+        
         if(req.url === '/') {
+            const url = new UrlChecker(req.url)
+            url.url = '123'
+            console.log(url.url)
+
             res.writeHead(200, {"content-type" : 'text/plain; charset=utf8'})
             res.write('안녕하세요')
             res.end()
