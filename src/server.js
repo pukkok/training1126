8a250c6e7fea14d8308c7ea736b7aeeee596d1e1
pukkok <krystalgggg@gmail.com>
2024-11-26 14:56:46 +0900

    서버 작동시 확인을 위한 response 기초작업 추가

--- src/server.js
+++ src/server.js
@@ -3,7 +3,9 @@ import http from 'http'
 const server = http.createServer((req, res) => {
     if(req.method === 'GET') {
         if(req.url === '/') {
-            
+            res.writeHead(200, {"content-type" : 'text/plain; charset=utf8'})
+            res.write('안녕하세요')
+            res.end()
         }
     }
 
