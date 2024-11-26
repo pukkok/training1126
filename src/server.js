e3495448bb2237f51b925dd296754cd0794e09dd
pukkok <krystalgggg@gmail.com>
2024-11-26 14:35:24 +0900

    method가 get일때, url이 '/' 일때 (서버 초기 실행시 설정)

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 import http from 'http'
-import fs from 'fs'
 
 const server = http.createServer((req, res) => {
+    if(req.method === 'GET') {
+        if(req.url === '/') {
+            
+        }
+    }
 
 })
 
