9904762b057b62b629284cb3c8153512f8c3f778
pukkok <krystalgggg@gmail.com>
2024-11-27 16:02:38 +0900

    head, body 불러오기 테스트 완료

--- src/template/HTML.js
+++ src/template/HTML.js
@@ -1,20 +1,23 @@
-import Handlebars from "handlebars"
+import head from "./head.js"
+import body from "./body.js"
+import TemplateMaker from "./TemplateMaker.js"
 
-class HTML {
-    static source = 
+const headPart = new TemplateMaker(head.source, head.data)
+const bodyPart = new TemplateMaker(body.source, body.data)
+
+const HTML = {
+    source :
     `
     <!DOCTYPE html>
     <html lang="ko">
-    {{head}}
-    {{body}}
+    {{{head}}}
+    {{{body}}}
     </html>
-    `
-
-    build () {
-        const template = Handlebars.compile(HTML.source)
-        
+    `,
+    data : {
+        head : headPart.template(),
+        body : bodyPart.template()
     }
-
 }
 
 export default HTML
