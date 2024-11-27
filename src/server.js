c338a21b1ebefacd4769251f464c9e0c8eece696
pukkok <krystalgggg@gmail.com>
2024-11-27 16:57:03 +0900

    filewriter 테스트 완료

--- src/server.js
+++ src/server.js
@@ -1,16 +1,19 @@
-import http from 'http'
-import FileReader from './fileReader/FileReader.js'
-import {exec} from 'child_process'
-const server = http.createServer((req, res) => {
-    if(req.method === 'GET') {
-        const file = new FileReader(req.url, res)
-        file.read()
-    }
-})
+// import http from 'http'
+// import FileReader from './fileReader/FileReader.js'
+// import {exec} from 'child_process'
+// const server = http.createServer((req, res) => {
+//     if(req.method === 'GET') {
+//         const file = new FileReader(req.url, res)
+//         file.read()
+//     }
+// })
 
-server.listen(8080, () => {
-    // console.log('http://localhost:8080')
-    exec('start http://localhost:8080', () => {
-        console.log('서버 동작')
-    })
-})
+// server.listen(8080, () => {
+//     // console.log('http://localhost:8080')
+//     exec('start http://localhost:8080', () => {
+//         console.log('서버 동작')
+//     })
+// })
+
+import fileWriter from "./fileWriter/fileWriter.js"
+fileWriter()
