1c0985002af036420fee6bd77b32a61a0e640b02
pukkok <krystalgggg@gmail.com>
2024-11-27 17:12:44 +0900

    file 쓰고 읽어주기 테스트 완료

--- src/server.js
+++ src/server.js
@@ -1,19 +1,19 @@
-// import http from 'http'
-// import FileReader from './fileReader/FileReader.js'
-// import {exec} from 'child_process'
-// const server = http.createServer((req, res) => {
-//     if(req.method === 'GET') {
-//         const file = new FileReader(req.url, res)
-//         file.read()
-//     }
-// })
-
-// server.listen(8080, () => {
-//     // console.log('http://localhost:8080')
-//     exec('start http://localhost:8080', () => {
-//         console.log('서버 동작')
-//     })
-// })
-
+import http from 'http'
+import FileReader from './fileReader/FileReader.js'
 import fileWriter from "./fileWriter/fileWriter.js"
-fileWriter()
+
+import {exec} from 'child_process'
+const server = http.createServer((req, res) => {
+    if(req.method === 'GET') {
+        fileWriter()
+        const file = new FileReader(req.url, res)
+        file.read()
+    }
+})
+
+server.listen(8080, () => {
+    // console.log('http://localhost:8080')
+    exec('start http://localhost:8080', () => {
+        console.log('서버 동작')
+    })
+})
