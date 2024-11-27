bcb79e03e2c0698707b1c5df64c9126bf67c536f
Pukkok <krystalgggg@gmail.com>
2024-11-27 21:19:29 +0900

    div로 묶는 작업으로 변경

--- src/template/Body/Main.js
+++ src/template/Body/Main.js
@@ -1,23 +1,17 @@
 // #each 와 #with로 구현 가능할까
 
-// {{#with content}}
-//     <h1>{{main}}</h1>
-//     <p>{{sub}}</p>
-//     {{/with}}
-
-
 const main = {
     source : 
     `
     <main>
-    <div>
     {{#each test}}
+    <div id="{{this.main}}">
     {{#with this}}
     <h1>{{this.main}}</h1>
     <p>{{this.sub}}</p>
     {{/with}}
-    {{/each}}
     </div>
+    {{/each}}
     </main>
     `,
     data : {
@@ -34,11 +28,7 @@ const main = {
                 main : '공욱재',
                 sub : '심리학 개발자!'
             }
-        ],
-        // test : {
-        //     mainContent : ['김민지', '서민석', '공욱재'],
-        //     description : ['제이쿼리 개발자!', '리액트 개발자!', '심리학 개발자!']
-        // }
+        ]
     }
 }
 
