f8dc17291ea62aa228bdc1ef44944363f339d7ab
pukkok <krystalgggg@gmail.com>
2024-11-27 16:28:35 +0900

    제작된 template export로 변경

--- src/template/buildTemplate.js
+++ src/template/buildTemplate.js
@@ -2,5 +2,5 @@ import HTML from "./HTML.js"
 import TemplateMaker from "./TemplateMaker.js"
 
 const HtmlPart = new TemplateMaker(HTML.source, HTML.data)
-console.log(HtmlPart.template())
-
+// console.log(HtmlPart.template())
+export default HtmlPart
