5468005f0e7e975c7b39eb9dcdd2b9df6ba0000b
pukkok <krystalgggg@gmail.com>
2024-11-27 18:09:44 +0900

    반복동작 테스트 완료

--- src/template/Body/Main.js
+++ src/template/Body/Main.js
@@ -1,16 +1,44 @@
+// #each 와 #with로 구현 가능할까
+
+// {{#with content}}
+//     <h1>{{main}}</h1>
+//     <p>{{sub}}</p>
+//     {{/with}}
+
+
 const main = {
     source : 
     `
     <main>
     <div>
-    <h1>{{mainContent}}</h1>
-    <p>{{description}}</p>
+    {{#each test}}
+    {{#with this}}
+    <h1>{{this.main}}</h1>
+    <p>{{this.sub}}</p>
+    {{/with}}
+    {{/each}}
     </div>
     </main>
     `,
     data : {
-        mainContent : '서민석',
-        description : '리액트 개발자!'
+        test : [
+            {
+                main : '김민지',
+                sub : '제이쿼리 개발자!'
+            },
+            {
+                main : '서민석',
+                sub : '리액트 개발자!'
+            },
+            {
+                main : '공욱재',
+                sub : '심리학 개발자!'
+            }
+        ],
+        // test : {
+        //     mainContent : ['김민지', '서민석', '공욱재'],
+        //     description : ['제이쿼리 개발자!', '리액트 개발자!', '심리학 개발자!']
+        // }
     }
 }
 
