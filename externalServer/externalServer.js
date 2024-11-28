c8a58c1a9b4d66cd32425c19d50d849f217fdc82
pukkok <krystalgggg@gmail.com>
2024-11-28 16:39:59 +0900

    다른 요청을 받는 경우 추가

--- externalServer/externalServer.js
+++ externalServer/externalServer.js
@@ -7,6 +7,11 @@ const server = http.createServer((req, res) => {
             res.writeHead(200, {"content-type" : 'application/json'})
             res.end(JSON.stringify('신흥 세력 개발자!'))
         }
+
+        if(req.url === '/api/reset-data') {
+            res.writeHead(200, {"content-type" : "application/json"})
+            res.end(JSON.stringify('돌아온 제이쿼리 개발자!'))
+        }
     }
 
 })
