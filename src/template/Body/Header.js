783c64ffdbbff517f8bccf088c6abe0433dda90d
pukkok <krystalgggg@gmail.com>
2024-11-28 15:59:15 +0900

    요청 테스트를 위한 li a 추가

--- src/template/Body/Header.js
+++ src/template/Body/Header.js
@@ -3,6 +3,7 @@ const header = {
     `
     <header>
     <ul>
+    <li><a id="get-test" href=api/test-data>요청확인</a></li>
     {{#each item}}
     <li><a href=#{{this}}>{{this}}</a></li>
     {{/each}}
