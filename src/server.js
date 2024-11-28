e0d58244e58477ee6c13b73c2664bfa4bd213987
pukkok <krystalgggg@gmail.com>
2024-11-28 16:37:06 +0900

    요청에 따라 응답 데이터를 보내줄수 있도록 구현 테스트 완료

--- src/server.js
+++ src/server.js
@@ -4,16 +4,30 @@ import fileWriter from "./fileWriter/fileWriter.js"
 
 import {exec} from 'child_process'
 const server = http.createServer((req, res) => {
+    console.log(req.url)
     if(req.method === 'GET') {
-        fileWriter()
-        const file = new FileReader(req.url, res)
-        file.read()
+        if(['/', '/index.js', '/style.css'].includes(req.url)) {
+            fileWriter()
+            const file = new FileReader(req.url, res)
+            file.read()
+        }
+
+        if(req.url === '/api/test-data') {
+            fetch('http://localhost:8080/api/test-data')
+            .then(res => res.json())
+            .then(data => {
+                res.writeHead(200, {"content-type" : 'application/json'})
+                res.end(JSON.stringify(data))
+            })
+        }
     }
+
+    
 })
 
-server.listen(8080, () => {
-    // console.log('http://localhost:8080')
-    exec('start http://localhost:8080', () => {
+server.listen(5000, () => {
+    // console.log('http://localhost:5000')
+    exec('start http://localhost:5000', () => {
         console.log('서버 동작')
     })
 })
