d39eb2838ac88e9421fc83670bd0eed2fdcd0496
pukkok <krystalgggg@gmail.com>
2024-11-28 16:41:58 +0900

    값에 따라 다른 요청을 받을수 있도록 구현

--- public/index.js
+++ public/index.js
@@ -2,14 +2,24 @@ console.log('읽기 완료')
 
 window.addEventListener('DOMContentLoaded', () => {
     const firstAnchor = document.getElementById('get-test')
+    const minji = document.getElementById('김민지')
+    const minjiP = minji.querySelector('p')
+
     firstAnchor.addEventListener('click' , () => {
-        fetch('/api/test-data')
-        .then(res => res.json())
-        .then(data => {
-            const minji = document.getElementById('김민지')
-            const minjiP = minji.querySelector('p')
-            let result = data
-            minjiP.innerHTML = result
-        })
+        if(minjiP.innerText === '제이쿼리 개발자!') {
+            fetch('/api/test-data')
+            .then(res => res.json())
+            .then(data => {
+                let result = data
+                minjiP.innerHTML = result
+            })
+        } else {
+            fetch('/api/reset-data')
+            .then(res => res.json())
+            .then(data => {
+                let result = data
+                minjiP.innerHTML = result
+            })
+        }
     })
 })
