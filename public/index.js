ddfa240b7a58e80fb75478e926a13b6effd5ffaf
pukkok <krystalgggg@gmail.com>
2024-11-28 16:00:10 +0900

    요청 테스트를 위한 fetch 작업 추가

--- public/index.js
+++ public/index.js
@@ -1,1 +1,15 @@
 console.log('읽기 완료')
+
+window.addEventListener('DOMContentLoaded', () => {
+    const firstAnchor = document.getElementById('get-test')
+    firstAnchor.addEventListener('click' , () => {
+        fetch('/api/test-data')
+        .then(res => res.json())
+        .then(data => {
+            const minji = document.getElementById('김민지')
+            const minjiP = minji.querySelector('p')
+            let result = data
+            minjiP.innerHTML = result
+        })
+    })
+})
